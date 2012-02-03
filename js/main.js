3352f2363a29d566f5ffb9b5d9f0776d16584b84
ukyo <ukyo.web@gmail.com>
2012-02-04 07:00:46 +0900

    0.1 beta

--- js/main.js
+++ js/main.js
@@ -1,65 +1,142 @@
+/**
+ * author: Syu Kato, ukyo.web@gmail.com, @ukyo
+ */
+
+
+/**
+ * Extract AAC from movie.
+ * 
+ * @param {ArrayBuffer} buffer
+ * @param {string} title
+ */
 function extractAAC(buffer, title){
-    var mp4 = new Mp4(buffer);
-    fs.update({
-        size: 100 * 1024 * 1024,
-        name: title + ".aac",
-        data: mp4.extractAAC(),
-        success: function(entry){
-            chrome.tabs.create({
-                url: entry.toURL('application/octet-stream')
-            });
-        }
-    });
+	var mp4 = new Mp4(buffer);
+	fs.update({
+		size: 100 * 1024 * 1024,
+		name: title + ".aac",
+		data: mp4.extractAAC(),
+		success: function(entry){
+			downloadFile({url: entry.toURL('audio/aac'), filename: title + '.aac'});
+		}
+	});
 }
 
+
+/**
+ * Extract mp3 from movie.
+ * 
+ * @param {ArrayBuffer} buffer
+ * @param {string} title
+ */
 function extractMp3(buffer, title){
-    var swf = new Swf(buffer);
-    fs.update({
-        size: 100 * 1024 * 1024,
-        name: title + ".mp3",
-        data: swf.extractMp3(),
-        success: function(entry){
-            chrome.tabs.create({
-                url: entry.toURL('application/octet-stream')
-            });
-        }
-    });
+	var swf = new Swf(buffer);
+	fs.update({
+		size: 1024 * 1024 * 1024,
+		name: title + ".mp3",
+		data: swf.extractMp3(),
+		success: function(entry){
+			downloadFile({url: entry.toURL('audio/mp3'), filename: title + '.mp3'});
+		}
+	});
 }
 
+
+/**
+ * Create a dummy tab and download a file to download folder.
+ * 
+ * @param {Object} obj file's url and name.
+ */
+function downloadFile(obj){
+	chrome.tabs.create({url: "../html/dummy.html", selected: false}, function(tab){
+		chrome.tabs.sendRequest(tab.id, obj, function(response){
+			console.log(response);
+		});
+	});
+}
+
+
+/**
+ * Extract audio from movie.
+ * If a type of movie is mp4, call function extractAAC.
+ * Else if a type of movie is swf, call function extractMp3.
+ */
 function extractAudio(){
-    chrome.tabs.getSelected(null, function(tab){
-        var url = getMovieURL(tab.url),
-            fn;
-        if(isSwf(tab.url.split("/").pop())){
-            fn = extractMp3;
-        } else {
-            fn = extractAAC;
-        }
-        loadFileBuffer(url, function(buffer){
-            fn(buffer, tab.title);
-        });
-    });
+	chrome.tabs.getSelected(null, function(tab){
+		try{
+			loadFileBuffer(getMovieURL(tab.url), function(buffer){
+				try{
+					(isSwf(tab.url.split("/").pop()) ? extractMp3 : extractAAC)(buffer, tab.title);
+				} catch (e) {
+					onerror();
+				}
+			});
+		} catch (e) {
+			onerror();
+		}
+	});
 }
 
+
+function onerror(){
+	var notification = createNotification("Nico Audio Extractor", "To extract was failed.");
+	notification.show();
+	setTimeout(notification.cancel, 5000);
+}
+
+
+/**
+ * @param {string} id
+ * @return {boolean}
+ */
 function isSwf(id){
-    return id.slice(0, 2) === "nm";
+	return id.slice(0, 2) === "nm";
 }
 
+
+/**
+ * get a movie data url with nicovideo API.
+ * 
+ * @param {string} pageURL
+ * @return {string} 動画データのURL
+ */
 function getMovieURL(pageURL){
-    var id = pageURL.split("/").pop();
-    var xhr2 = new XMLHttpRequest();
-    xhr2.open("GET", pageURL, false);
-    xhr2.send();
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://flapi.nicovideo.jp/api/getflv/" + id + (isSwf(id) ? "?as3=1" : ""), false);
-    xhr.send();
-    return decodeURIComponent(/url=([^&]+)/.exec(xhr.responseText)[1]);
+	var id = pageURL.split("/").pop();
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "http://flapi.nicovideo.jp/api/getflv/" + id + (isSwf(id) ? "?as3=1" : ""), false);
+	xhr.send();
+	return decodeURIComponent(/url=([^&]+)/.exec(xhr.responseText)[1]);
 }
 
-chrome.contextMenus.removeAll(function(){
-    chrome.contextMenus.create({
-        title: "Extract a Audio File",
-        contexts: ["all"],
-        onclick: extractAudio
-    });
-});
+
+/**
+ * Show Context Menu when you see the nicovideo page.
+ * 
+ * @param {number} tabId
+ * @param {Object} selectInfo
+ * @param {Tab} tab
+ */
+function showMenu(tabId, selectInfo, tab){
+	tab ? _showMenu(tab) : chrome.tabs.get(tabId, _showMenu);
+}
+
+
+/**
+ * Implementation of showMenu.
+ * 
+ * @param {Tab} tab
+ */
+function _showMenu(tab){
+	var rNico = /^https?:\/\/www.nicovideo\.jp\/watch.+$/;
+	chrome.contextMenus.removeAll(function(){
+		if(!rNico.test(tab.url)) return;
+		chrome.contextMenus.create({
+			title: "Extract Audio",
+			contexts: ["all"],
+			onclick: extractAudio
+		});
+	});
+}
+
+
+chrome.tabs.onUpdated.addListener(showMenu);
+chrome.tabs.onSelectionChanged.addListener(showMenu)
